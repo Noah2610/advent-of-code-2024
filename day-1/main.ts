8a0bf48d437a11606eeff7ee23f8d5513fa179a1
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2024-12-01 12:45:04 +0100

    day-1: Add day-1 part 2

--- day-1/main.ts
+++ day-1/main.ts
@@ -1,32 +1,47 @@
 async function main() {
     const data = await getInput();
-    const result = calcTotalDistance(data);
+    // const result = calcTotalDistance(data);
+    const result = calcSimilarityScore(data);
     console.log(result);
 }
 
+function calcSimilarityScore(data: string) {
+    const [listA, listB] = getLists(data);
+
+    return listA.reduce(
+        (score, num) =>
+            score +
+            num * listB.reduce((sum, b) => sum + (num === b ? 1 : 0), 0),
+        0,
+    );
+}
+
 function calcTotalDistance(data: string) {
-    const [listA, listB] = data
-        .split("\n")
-        .reduce<[number[], number[]]>(
-            (lists, line) => {
-                const nums = line.split(/\s+/).map(Number);
-                if (nums.length !== 2) {
-                    return lists;
-                }
-                return lists.map((list, i) => [...list, nums[i]]) as [
-                    number[],
-                    number[],
-                ];
-            },
-            [[], []],
-        )
-        .map((list) => list.sort((a, b) => a - b));
+    const [listA, listB] = getLists(data).map((list) =>
+        list.sort((a, b) => a - b),
+    );
 
     const totalDist = listA
         .map((numA, i) => [numA, listB[i]] as [number, number])
         .reduce((sum, [a, b]) => sum + Math.abs(a - b), 0);
 
     return totalDist;
+}
+
+function getLists(data: string) {
+    return data.split("\n").reduce<[number[], number[]]>(
+        (lists, line) => {
+            const nums = line.split(/\s+/).map(Number);
+            if (nums.length !== 2) {
+                return lists;
+            }
+            return lists.map((list, i) => [...list, nums[i]]) as [
+                number[],
+                number[],
+            ];
+        },
+        [[], []],
+    );
 }
 
 async function getInput() {
