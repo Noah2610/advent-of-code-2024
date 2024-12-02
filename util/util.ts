d887dc0fd4886bea5acfbbaacaaaea80eaf175aa
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2024-12-03 00:32:21 +0100

    Add optional dev option to getInput util function

--- util/util.ts
+++ util/util.ts
@@ -1,8 +1,14 @@
 import { dirname } from "node:path";
 
-export async function getInput(day: `day-${number}`): Promise<string> {
+export async function getInput(
+    day: `day-${number}`,
+    options?: { dev?: boolean },
+): Promise<string> {
+    const dev = options?.dev ?? false;
+    const inputFile = dev ? "dev-input.txt" : "input.txt";
+
     return await Deno.readTextFile(
-        `${dirname(import.meta.dirname ?? ".")}/${day}/input.txt`,
+        [dirname(import.meta.dirname ?? "."), day, inputFile].join("/"),
     );
 }
 
