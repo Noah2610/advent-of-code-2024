a08a79bf1d21326728ec2aaa1c78d6a3768d7da2
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2024-12-03 09:34:33 +0100

    Update getInput util to read dev-input.txt if DEV env is set

--- util/util.ts
+++ util/util.ts
@@ -1,10 +1,12 @@
 import { dirname } from "node:path";
 
+const IS_DEV_ENV = Deno.env.has("DEV");
+
 export async function getInput(
     day: `day-${number}`,
     options?: { dev?: boolean },
 ): Promise<string> {
-    const dev = options?.dev ?? false;
+    const dev = options?.dev ?? IS_DEV_ENV;
     const inputFile = dev ? "dev-input.txt" : "input.txt";
 
     return await Deno.readTextFile(
