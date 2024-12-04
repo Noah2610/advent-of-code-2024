3f3cfe73f85b219b5591a4ed9e7615d1ffb12220
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2024-12-04 16:39:29 +0100

    Update bin/run.ts to support rust projects

--- bin/run.ts
+++ bin/run.ts
@@ -1,6 +1,9 @@
 #!/bin/env -S deno --allow-read --allow-run
 
 import { dirname } from "node:path";
+import { expectNever } from "ts-expect";
+
+type ProjectType = "ts" | "rs";
 
 async function main(args: string[]) {
     if (args.length === 0) {
@@ -19,18 +22,40 @@ async function main(args: string[]) {
         throw new Error(`Day "${dayArg}" directory does not exist "${dir}"`);
     }
 
+    const projectType = await determineProjectType(dir);
     console.warn(`Running ${dayArg}`);
 
-    const cmd = new Deno.Command("deno", {
-        args: [
-            "run",
-            "--allow-read",
-            "--allow-env",
-            "main.ts",
-            ...args.slice(1),
-        ],
-        cwd: dir,
-    });
+    let cmd: Deno.Command;
+
+    switch (projectType) {
+        case "ts": {
+            cmd = new Deno.Command("deno", {
+                args: [
+                    "run",
+                    "--allow-read",
+                    "--allow-env",
+                    "main.ts",
+                    ...args.slice(1),
+                ],
+                cwd: dir,
+            });
+
+            break;
+        }
+
+        case "rs": {
+            cmd = new Deno.Command("cargo", {
+                args: ["run", ...args.slice(1)],
+                cwd: dir,
+            });
+
+            break;
+        }
+
+        default: {
+            expectNever(projectType);
+        }
+    }
 
     const proc = cmd.spawn();
     const status = await proc.status;
@@ -38,6 +63,34 @@ async function main(args: string[]) {
     if (!status.success) {
         throw new Error(`Process exited with status code ${status.code}`);
     }
+}
+
+async function determineProjectType(dir: string): Promise<ProjectType> {
+    const projectTypeFileNames: { file: string; type: ProjectType }[] = [
+        {
+            file: "main.ts",
+            type: "ts",
+        },
+        {
+            file: "Cargo.toml",
+            type: "rs",
+        },
+    ];
+
+    for (const { file, type } of projectTypeFileNames) {
+        try {
+            const stat = await Deno.stat([dir, file].join("/"));
+            if (!stat.isFile) {
+                throw new Error();
+            }
+
+            return type;
+        } catch (_e: unknown) {
+            // do nothing
+        }
+    }
+
+    throw new Error(`Failed to determine project type in dir "${dir}"`);
 }
 
 try {
